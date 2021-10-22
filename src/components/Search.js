6067b89c584db594f8d203e65637a15704de99f3
Sayujya Malkan <sayujyamalkan@gmail.com>
2021-10-23 00:06:17 +0530

    semi-final

---
+++ src/components/Search.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { MdSearch } from 'react-icons/md';
+
+const Search = ({ handleSearchNote }) => {
+    return (
+        <div className="search">
+            <MdSearch className="search-icon" size="1.3rem"/>
+            <input
+                type="text"
+                placeholder="Type to search..."
+                onChange={(event) => {handleSearchNote(event.target.value)}}
+            />
+        </div>
+    );
+}
+
+export default Search;
